3884841ef21138c91d3f968e7a65f91b3f315c33
anuj-pal27 <anujpal27669@gmail.com>
2025-04-27 14:06:40 +0530

    client url joining

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app=express()
 //     // credentials: true
 // }))
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:`${process.env.FRONTEND_URL}`,
     credentials:true
 }))
 
